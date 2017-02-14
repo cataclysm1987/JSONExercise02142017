ce59331712e7d74704f5b80b11f1b28aba11916b
cataclysm1987 <conklin.eric@gmail.com>
2017-02-14 16:20:55 -0500

    update json and getjson method

---
+++ scripts/json.js
@@ -0,0 +1,316 @@
+﻿var users = {
+    "_id": "58a3486f6c701948757d283c",
+    "index": 0,
+    "guid": "40bb75bd-5b72-4351-b3af-fbe8d08b415b",
+    "isActive": true,
+    "balance": "$1,109.50",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "eyeColor": "green",
+    "name": "Leigh Lott",
+    "gender": "female",
+    "company": "OPTICON",
+    "email": "leighlott@opticon.com",
+    "phone": "+1 (879) 447-3419",
+    "address": "427 Kent Street, Benson, Federated States Of Micronesia, 5903",
+    "about": "Ex minim voluptate ut enim proident ipsum qui fugiat in exercitation nisi. Proident culpa magna aliqua tempor voluptate nulla excepteur. Veniam qui cillum id deserunt. Dolor consectetur reprehenderit officia ea. Enim tempor cillum ad nostrud sit nostrud culpa consequat excepteur sit aute ipsum nulla.\r\n",
+    "registered": "2014-02-06T05:18:56 +05:00",
+    "latitude": 83.263485,
+    "longitude": 80.828482,
+    "tags": [
+      "pariatur",
+      "do",
+      "mollit",
+      "do",
+      "occaecat",
+      "id",
+      "culpa"
+    ],
+    "friends": [
+      {
+          "id": 0,
+          "name": "Shannon Solis"
+      },
+      {
+          "id": 1,
+          "name": "Adela Hinton"
+      },
+      {
+          "id": 2,
+          "name": "Conway Shaffer"
+      }
+    ],
+    "greeting": "Hello, Leigh Lott! You have 1 unread messages.",
+    "favoriteFruit": "banana"
+},
+{
+    "_id": "58a3486fca15058709b2dc15",
+    "index": 1,
+    "guid": "050b4b28-abdc-4172-9fdc-5d84c2f37db5",
+    "isActive": true,
+    "balance": "$1,505.15",
+    "picture": "http://placehold.it/32x32",
+    "age": 31,
+    "eyeColor": "blue",
+    "name": "Hollie Cameron",
+    "gender": "female",
+    "company": "FOSSIEL",
+    "email": "holliecameron@fossiel.com",
+    "phone": "+1 (934) 418-2416",
+    "address": "727 Nixon Court, Geyserville, New Hampshire, 3991",
+    "about": "Veniam ad minim culpa consectetur est adipisicing enim commodo tempor enim cillum labore. Nisi incididunt quis do dolore cupidatat in. In laboris fugiat ex laborum.\r\n",
+    "registered": "2016-10-01T05:53:01 +05:00",
+    "latitude": -57.390004,
+    "longitude": -11.727436,
+    "tags": [
+      "dolor",
+      "do",
+      "cillum",
+      "Lorem",
+      "consequat",
+      "qui",
+      "est"
+    ],
+    "friends": [
+      {
+          "id": 0,
+          "name": "Poole Hendricks"
+      },
+      {
+          "id": 1,
+          "name": "Green Vance"
+      },
+      {
+          "id": 2,
+          "name": "Jordan Ramirez"
+      }
+    ],
+    "greeting": "Hello, Hollie Cameron! You have 10 unread messages.",
+    "favoriteFruit": "apple"
+},
+{
+    "_id": "58a3486f413818d659cb96e7",
+    "index": 2,
+    "guid": "8b0e671b-4d91-4bb5-b9ef-f8aa4118c77b",
+    "isActive": false,
+    "balance": "$1,433.98",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "green",
+    "name": "Christensen Shelton",
+    "gender": "male",
+    "company": "ROBOID",
+    "email": "christensenshelton@roboid.com",
+    "phone": "+1 (925) 478-2932",
+    "address": "784 Coleman Street, Groveville, Northern Mariana Islands, 3683",
+    "about": "Mollit laboris enim sint quis deserunt qui exercitation reprehenderit nostrud magna nulla reprehenderit exercitation. Elit adipisicing Lorem est quis dolore non velit labore voluptate aute et. Fugiat eu ad sint veniam qui. Ullamco non laborum cillum nisi mollit veniam ut in pariatur.\r\n",
+    "registered": "2015-08-01T02:56:38 +05:00",
+    "latitude": -38.881014,
+    "longitude": -15.855689,
+    "tags": [
+      "nostrud",
+      "exercitation",
+      "amet",
+      "sunt",
+      "excepteur",
+      "ex",
+      "occaecat"
+    ],
+    "friends": [
+      {
+          "id": 0,
+          "name": "Morris Cleveland"
+      },
+      {
+          "id": 1,
+          "name": "Sasha Macdonald"
+      },
+      {
+          "id": 2,
+          "name": "Livingston Durham"
+      }
+    ],
+    "greeting": "Hello, Christensen Shelton! You have 3 unread messages.",
+    "favoriteFruit": "banana"
+},
+{
+    "_id": "58a3486f80ec9f48e2ce7194",
+    "index": 3,
+    "guid": "247d4ff9-3716-4cfa-b9f4-b3bd2fe9371e",
+    "isActive": false,
+    "balance": "$1,826.18",
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "brown",
+    "name": "Marcy Haney",
+    "gender": "female",
+    "company": "ACCUFARM",
+    "email": "marcyhaney@accufarm.com",
+    "phone": "+1 (801) 480-2909",
+    "address": "775 Stillwell Avenue, Holcombe, West Virginia, 5375",
+    "about": "Ea duis non quis laborum occaecat veniam. Nisi ullamco officia aliquip dolor excepteur. Ex laborum fugiat nostrud dolor eu nulla culpa. Voluptate exercitation tempor deserunt tempor est ut occaecat. Dolore irure labore occaecat sit ipsum proident laborum esse laborum amet est sint consequat velit. Nisi consequat aliqua ad quis in dolor fugiat enim nulla.\r\n",
+    "registered": "2017-02-08T04:50:56 +05:00",
+    "latitude": 77.662211,
+    "longitude": 68.832919,
+    "tags": [
+      "sunt",
+      "exercitation",
+      "aliqua",
+      "laborum",
+      "commodo",
+      "magna",
+      "consequat"
+    ],
+    "friends": [
+      {
+          "id": 0,
+          "name": "Aileen Kelly"
+      },
+      {
+          "id": 1,
+          "name": "Rochelle Douglas"
+      },
+      {
+          "id": 2,
+          "name": "Sweet Carter"
+      }
+    ],
+    "greeting": "Hello, Marcy Haney! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+},
+{
+    "_id": "58a3486f75ae2b7ceb2673ee",
+    "index": 4,
+    "guid": "96a6c5fc-377e-4d39-a51c-1b83b83cd274",
+    "isActive": true,
+    "balance": "$3,526.40",
+    "picture": "http://placehold.it/32x32",
+    "age": 24,
+    "eyeColor": "brown",
+    "name": "Mccoy Rivers",
+    "gender": "male",
+    "company": "APEX",
+    "email": "mccoyrivers@apex.com",
+    "phone": "+1 (988) 418-2649",
+    "address": "883 Scott Avenue, Draper, Vermont, 8124",
+    "about": "Excepteur Lorem eu fugiat minim fugiat quis nulla ex dolore laborum duis elit magna et. Pariatur aliqua enim proident non consequat. Eiusmod amet id pariatur adipisicing ea ipsum ullamco. Proident esse aute nostrud nostrud laboris ipsum laboris veniam do dolor labore amet. Aliqua labore tempor fugiat dolor occaecat proident laborum.\r\n",
+    "registered": "2016-04-10T11:33:27 +05:00",
+    "latitude": -2.974966,
+    "longitude": -24.163257,
+    "tags": [
+      "minim",
+      "aliqua",
+      "aliquip",
+      "aliquip",
+      "elit",
+      "enim",
+      "magna"
+    ],
+    "friends": [
+      {
+          "id": 0,
+          "name": "Chasity Clark"
+      },
+      {
+          "id": 1,
+          "name": "Carolyn Winters"
+      },
+      {
+          "id": 2,
+          "name": "Sheena Jacobs"
+      }
+    ],
+    "greeting": "Hello, Mccoy Rivers! You have 10 unread messages.",
+    "favoriteFruit": "apple"
+},
+{
+    "_id": "58a3486f012606078194265e",
+    "index": 5,
+    "guid": "e0fdbe63-1bef-4c54-9847-5d620a7a5908",
+    "isActive": true,
+    "balance": "$1,904.68",
+    "picture": "http://placehold.it/32x32",
+    "age": 25,
+    "eyeColor": "green",
+    "name": "Castillo Hurst",
+    "gender": "male",
+    "company": "PLEXIA",
+    "email": "castillohurst@plexia.com",
+    "phone": "+1 (975) 432-3391",
+    "address": "447 Cooke Court, Yettem, Indiana, 6146",
+    "about": "Dolor consectetur minim ipsum Lorem duis velit non aute consequat mollit aute enim. Mollit sit magna eiusmod magna officia. Excepteur duis occaecat adipisicing elit incididunt ipsum voluptate deserunt do laborum.\r\n",
+    "registered": "2015-07-15T07:37:57 +05:00",
+    "latitude": 39.348824,
+    "longitude": 82.485759,
+    "tags": [
+      "elit",
+      "cillum",
+      "sunt",
+      "ullamco",
+      "labore",
+      "incididunt",
+      "duis"
+    ],
+    "friends": [
+      {
+          "id": 0,
+          "name": "Johnnie Ellis"
+      },
+      {
+          "id": 1,
+          "name": "Ellis Williamson"
+      },
+      {
+          "id": 2,
+          "name": "Toni Sweeney"
+      }
+    ],
+    "greeting": "Hello, Castillo Hurst! You have 8 unread messages.",
+    "favoriteFruit": "strawberry"
+},
+{
+    "_id": "58a3486f1eba2d5005ff1118",
+    "index": 6,
+    "guid": "74e4e8af-257e-44b7-81d1-2025dad009a1",
+    "isActive": true,
+    "balance": "$3,755.84",
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "blue",
+    "name": "Hicks French",
+    "gender": "male",
+    "company": "COMBOGENE",
+    "email": "hicksfrench@combogene.com",
+    "phone": "+1 (854) 504-2494",
+    "address": "844 Wallabout Street, Cumberland, Maryland, 4593",
+    "about": "Eiusmod Lorem dolore pariatur magna magna aute eiusmod. Dolor eiusmod nostrud adipisicing est elit ipsum ea non quis labore minim consequat nostrud. Sint ad incididunt eiusmod cillum magna exercitation dolor quis cupidatat laboris laborum.\r\n",
+    "registered": "2015-11-11T11:52:42 +05:00",
+    "latitude": -84.4642,
+    "longitude": -139.919845,
+    "tags": [
+      "enim",
+      "et",
+      "ex",
+      "minim",
+      "aliquip",
+      "duis",
+      "in"
+    ],
+    "friends": [
+      {
+          "id": 0,
+          "name": "Lorrie Calhoun"
+      },
+      {
+          "id": 1,
+          "name": "Katy Sloan"
+      },
+      {
+          "id": 2,
+          "name": "Campos Beasley"
+      }
+    ],
+    "greeting": "Hello, Hicks French! You have 2 unread messages.",
+    "favoriteFruit": "apple"
+}
+
