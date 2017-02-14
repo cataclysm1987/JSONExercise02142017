3cf17c5f1df250b4945bf51cd8e38b73ba74323e
cataclysm1987 <conklin.eric@gmail.com>
2017-02-14 14:10:37 -0500

    initial model

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+﻿function TestGetJSON() {$.getJSON("users.json", function (data) {
+    var items = [];
+    $.each(data, function (key, val) {
+        items.push("<li id='" + key + "'>" + val + "</li>");
+    });
+
+    $("<ul/>", {
+        "class": "my-new-list",
+        html: items.join("")
+    }).appendTo("body");
+});
+}
+
+
+function AccessUserName() {$.getJSON("users.js", function (json) {
+    console.log("JSON Data: " + json.users[3].name);
+});}
